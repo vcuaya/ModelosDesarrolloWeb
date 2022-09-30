2d6e270d898328228d6b1dd4e003dacc0427f130
Víctor Cuaya <vcuaya@outlook.com>
2022-09-30 03:26:23 -0400

    Archivo auxiliar con la configuración correcta antes de probar el uso de las Cookies

--- aux.py
+++ aux.py
@@ -1,14 +1,30 @@
-from random import random
-import sys
-import os
+#!/usr/bin/env python3
 
-# Get path to mymodule
-scriptPath = os.path.dirname(__file__)
-modulePath = os.path.join(scriptPath, 'p04', 'img')
-sys.path.append(modulePath)
-import images
+# HTML is following
+print('Content-Type: text/html')
 
-list = images.get()
-random.shuffle(list)
+# Leave a blank line
+print('')
 
-print(list)
+# Format text in HTML document
+def plantilla(title):
+    print("""
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>""" + title + """</title>
+  <link href="./<p00>/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <p>Hello world</p>
+  <script src="./<p00>/js/script.js"></script>
+</body>
+
+</html>
+""")
+
+plantilla("CGI")
