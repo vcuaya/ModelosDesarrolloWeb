2a494d89399b844c8bf6d11d5561694886736354
Víctor Cuaya <vcuaya@outlook.com>
2022-10-04 17:13:12 -0400

    Primera prueba exitosa de la creación de cookies

--- cgi-enabled/index.py
+++ cgi-enabled/index.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
-import os
 import cgi
-import sys
-import random
+import requests
+from http import cookies
 
-# Get path to my module
-scriptPath = os.path.dirname(__file__)
-modulePath = os.path.join(scriptPath, '..', 'p04', 'img')
-sys.path.append(modulePath)
-import images as img
+# Cookies
+C = cookies.SimpleCookie()
+C["fig"] = "newton"
+C["sugar"] = "wafer"
+
+session=requests.Session()
+print(session.cookies.get_dict())
+print(C)  # generate HTTP headers
+
+# print(C.output())  # same thing
 
 # HTML is following
 print('Content-Type: text/html')
@@ -17,110 +21,48 @@ print('Content-Type: text/html')
 # Leave a blank line
 print('')
 
-# Variable to get values from send it form
-form = cgi.FieldStorage()
-page = form.getvalue("page")
 
-# Format text in HTML document
-def template(title, table):
+def plantilla(title):
     print("""
 <!DOCTYPE html>
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	<title>""" + title + """</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  <link href="./../p04/css/style.css" rel="stylesheet" type="text/css" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>""" + title + """</title>
+  <link href="./<p00>/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body class="bg-dark">
-	<div class="table-responsive">
-		<table class="table table-dark">
-			<thead>
-				<tr>
-					<th class="text-center thead-font-border" colspan="5">""" + title + """</th>
-				</tr>
-			</thead>
-			<tbody>
-    """ + table + """
-			</tbody>
-			<tfoot>
-				<tr>
-					<th class="text-center tfoot-font-border" colspan="2">Puntaje</th>
-				</tr>
-			</tfoot>
-		</table>
-	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-		crossorigin="anonymous"></script>
+<body>
+  <p>Hello world</p>
+  <script src="./<p00>/js/script.js"></script>
 </body>
 
 </html>
 """)
 
 
-def getCards():
-    # Get images from images module
-    aux = img.get()
+""" C = cookies.SimpleCookie()
+C.load("chips=ahoy; vienna=finger")  # load from a string (HTTP header)
+print(C)
 
-    # Shuffle aux array
-    random.shuffle(aux)
+C = cookies.SimpleCookie()
+C.load('keebler="E=everybody; L=\\"Loves\\"; fudge=\\012;";')
+print(C)
 
-    # Cards array for use in the boardgame
-    basenames = []
+C = cookies.SimpleCookie()
+C["oreo"] = "doublestuff"
+C["oreo"]["path"] = "/"
+print(C)
 
-    # Append images into cards
-    for x in range(10):
-        basenames.append(aux.pop())
-
-    # Duplicate cards array
-    basenames.extend(basenames)
-
-    # Shuffle cards array
-    random.shuffle(basenames)
-
-    return basenames
-
-
-def table(flipped, cards):
-    board = cards
-    for x in range(20):
-        if not flipped[x]:
-          board[x]='200px-NAP-01_Back.png'
-    board.reverse()
-    # String to append
-    string = "\n"
-    for i in range(4):
-        string += '\t<tr>\n'
-        for j in range(5):
-            string += '\t\t'+'<td><img class="img-thumbnail img-fluid" src="./../p04/img/' + \
-                str(board.pop())+'"></td>\n'
-        string += '\t</tr>\n'
-    return string
-
-
-""" flipped = [
-    False, False, False, False, False,
-    False, False, False, False, False,
-    False, False, False, False, False,
-    False, False, False, False, False
-] """
-
-flipped = [
-    True, True, False, False, False,
-    False, False, False, False, False,
-    False, False, False, False, False,
-    False, False, False, False, True
-]
-
-# Tarjetas del tablero
-cards = getCards()
-
-# Imágenes para la plantilla
-images = table(flipped, cards)
-
-# Impresión de la página
-template("Golden Stars Memorama", images)
+C = cookies.SimpleCookie()
+C["twix"] = "none for you"
+C["twix"].value
+C = cookies.SimpleCookie()
+C["number"] = 7  # equivalent to C["number"] = str(7)
+C["string"] = "seven"
+C["number"].value
+C["string"].value
+print(C)
+ """
