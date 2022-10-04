e186c9b6a8e32284cacbd1576b40ec80f3adf2d9
Víctor Cuaya <vcuaya@outlook.com>
2022-10-04 17:08:21 -0400

    Se respalda el avance del proyecto para hacer pruebas con las cookies

--- aux.py
+++ aux.py
@@ -10,8 +10,6 @@ scriptPath = os.path.dirname(__file__)
 modulePath = os.path.join(scriptPath, '..', 'p04', 'img')
 sys.path.append(modulePath)
 import images as img
-# To get all images
-# images.get()
 
 # HTML is following
 print('Content-Type: text/html')
@@ -20,8 +18,8 @@ print('Content-Type: text/html')
 print('')
 
 # Variable to get values from send it form
-#form = cgi.FieldStorage()
-#page = form.getvalue("page")
+form = cgi.FieldStorage()
+page = form.getvalue("page")
 
 # Format text in HTML document
 def template(title, table):
@@ -30,36 +28,41 @@ def template(title, table):
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>""" + title + """</title>
-  <link href="./<p00>/css/style.css" rel="stylesheet" type="text/css" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<title>""" + title + """</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="./../p04/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
-  <table>
-    <thead>
-      <tr>
-        <th colspan="5">Golden Stars Memorama</th>
-      </tr>
-    </thead>
-    <tbody>
+<body class="bg-dark">
+	<div class="table-responsive">
+		<table class="table table-dark">
+			<thead>
+				<tr>
+					<th class="text-center thead-font-border" colspan="5">""" + title + """</th>
+				</tr>
+			</thead>
+			<tbody>
     """ + table + """
-    </tbody>
-      <tfoot>
-        <tr>
-          <th colspan="5">Puntaje</th>
-        </tr>
-      </tfoot>
-    </table>
-  <script src="./<p00>/js/script.js"></script>
+			</tbody>
+			<tfoot>
+				<tr>
+					<th class="text-center tfoot-font-border" colspan="2">Puntaje</th>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+		crossorigin="anonymous"></script>
 </body>
 
 </html>
 """)
 
-
-def table():
+    
+def getCards():
     # Get images from images module
     aux = img.get()
 
@@ -67,28 +70,57 @@ def table():
     random.shuffle(aux)
 
     # Cards array for use in the boardgame
-    cards = []
+    basenames = []
 
     # Append images into cards
     for x in range(10):
-        cards.append(aux.pop())
+        basenames.append(aux.pop())
 
     # Duplicate cards array
-    cards.extend(cards)
+    basenames.extend(basenames)
 
     # Shuffle cards array
-    random.shuffle(cards)
+    random.shuffle(basenames)
 
+    return basenames
+
+
+def table(flipped, cards):
+    board = cards
+    for x in range(20):
+        if not flipped[x]:
+          board[x]='200px-NAP-01_Back.png'
+    board.reverse()
     # String to append
-    answer = "\n"
+    string = "\n"
     for i in range(4):
-        answer += '\t<tr>\n'
+        string += '\t<tr>\n'
         for j in range(5):
-            answer += '\t\t'+str(cards.pop())
-        answer += '\t</tr>\n'
+            string += '\t\t'+'<td><img class="img-thumbnail img-fluid" src="./../p04/img/' + \
+                str(board.pop())+'"></td>\n'
+        string += '\t</tr>\n'
+    return string
 
-    return answer
 
-t=table()
+""" flipped = [
+    False, False, False, False, False,
+    False, False, False, False, False,
+    False, False, False, False, False,
+    False, False, False, False, False
+] """
 
-template("CGI", t)
+flipped = [
+    True, True, False, False, False,
+    False, False, False, False, False,
+    False, False, False, False, False,
+    False, False, False, False, True
+]
+
+# Tarjetas del tablero
+cards = getCards()
+
+# Imágenes para la plantilla
+images = table(flipped, cards)
+
+# Impresión de la página
+template("Golden Stars Memorama", images)
