5a5cec839c0dd05812c86e225e85905cd1f95f29
Mars <m.a.r.s.232001@gmail.com>
2022-11-09 02:25:49 -0600

    Ya funciona todo tecnicamente

--- p05/js/script.js
+++ p05/js/script.js
@@ -1,52 +1,37 @@
-/* $(document).ready(() => {
-    $('.card').on('click', () => {
-        let id = $('.card').attr('id');
-        console.log(id);
-
+function devolverCarta(x,y) {
+    return new Promise(resolve => {
+      setTimeout(() => {
         $.ajax({
-            url: "./server.php",
+            url: "./backend/verifica.php",
             type: "GET",
-            data: { card: id },
+            data: {
+                card1: x,
+                card2: y
+            },
             success: function (response) {
                 console.log('Si entro');
                 console.log(response);
+                if (response)
+                    resolve(0)
+                else
+                    resolve(1)
             },
             error: function () {
                 console.log('No funcionó');
             }
         });
-    });
-}); */
-
-function devolverCarta(x,y) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        //const je = '<img class="img-thumbnail img-fluid" src="./img/gato.png"></a>'
-        /* $.ajax({
-            url: "./server.php",
-            type: "GET",
-            data: { card: x },
-            success: function (response) {
-                console.log('Si entro');
-                resolve(response);
-            },
-            error: function () {
-                console.log('No funcionó');
-            }
-        }); */
-        resolve(1)
         
       }, 2000);
     });
-  }
+}
 
 async function carta2(id, id2) {
     console.log(id)
     console.log(id2)
     const siOno = await devolverCarta(id, id2);
     if(siOno==1){
-        document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/200px-NAP-01_Back.png>'
-        document.getElementById(id2).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/200px-NAP-01_Back.png>'
+        document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src="./img/200px-NAP-01_Back.png">'
+        document.getElementById(id2).innerHTML = '<img class="img-thumbnail img-fluid" src="./img/200px-NAP-01_Back.png">'
     }
     a =''
     b =''
@@ -58,13 +43,13 @@ var b =''
 function carta(id) {
     if(a==''|| b==''){
         $.ajax({
-            url: "./server.php",
+            url: "./backend/server.php",
             type: "GET",
             data: { card: id},
             success: function (response) {
                 console.log('Si entro');
                 //resolve(response);
-                document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/'+response+'>'
+                document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./img/'+response+'>'
             },
             error: function () {
                 console.log('No funcionó');
@@ -84,7 +69,7 @@ function carta(id) {
     }
     
 }
-$(document).ready(() => {
+/* $(document).ready(() => {
     let id1 = 0;
     // let id2 = 1;
     let id2 = 10;
@@ -107,4 +92,4 @@ $(document).ready(() => {
             console.log('No funcionó');
         }
     });
-});
+}); */
