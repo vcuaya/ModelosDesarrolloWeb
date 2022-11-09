769a869f774f87ffb8bc5ba6ed981b75e898dda4
Mars <m.a.r.s.232001@gmail.com>
2022-11-09 01:46:34 -0600

    Nose volteen mas de dos cartas

--- p05/backend/script.js
+++ p05/backend/script.js
@@ -18,11 +18,11 @@
     });
 }); */
 
-/* function devolverCarta(x) {
+function devolverCarta(x,y) {
     return new Promise(resolve => {
       setTimeout(() => {
         //const je = '<img class="img-thumbnail img-fluid" src="./img/gato.png"></a>'
-        $.ajax({
+        /* $.ajax({
             url: "./server.php",
             type: "GET",
             data: { card: x },
@@ -33,31 +33,54 @@
             error: function () {
                 console.log('No funcionó');
             }
-        });
+        }); */
+        resolve(1)
         
       }, 2000);
     });
-  } */
+  }
 
-/* async function carta(id) {
-    document.getElementById(id).innerHTML = "Hiciste click";
+async function carta2(id, id2) {
     console.log(id)
-    const a = await devolverCarta(id);
-    document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/'+a+'>'
-} */
+    console.log(id2)
+    const siOno = await devolverCarta(id, id2);
+    if(siOno==1){
+        document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/200px-NAP-01_Back.png>'
+        document.getElementById(id2).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/200px-NAP-01_Back.png>'
+    }
+    a =''
+    b =''
+}
 
-async function carta(id) {
-    $.ajax({
-        url: "./server.php",
-        type: "GET",
-        data: { card: id},
-        success: function (response) {
-            console.log('Si entro');
-            //resolve(response);
-            document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/'+response+'>'
-        },
-        error: function () {
-            console.log('No funcionó');
-        }
-    });
+var a =''
+var b =''
+
+function carta(id) {
+    if(a==''|| b==''){
+        $.ajax({
+            url: "./server.php",
+            type: "GET",
+            data: { card: id},
+            success: function (response) {
+                console.log('Si entro');
+                //resolve(response);
+                document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/'+response+'>'
+            },
+            error: function () {
+                console.log('No funcionó');
+            }
+        });
+    }
+    if(a!='' && b==''){
+        b = id
+        console.log("entro b")
+    }
+    if(a==''){
+        a = id
+        console.log("entro a")
+    }
+    if(a!='' && b!=''){
+        carta2(a,b)  
+    }
+    
 }
