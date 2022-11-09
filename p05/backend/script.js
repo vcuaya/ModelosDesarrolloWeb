e14b14a51d8ef002f5ccf347b29953e301fabb34
Mars <m.a.r.s.232001@gmail.com>
2022-11-09 00:36:02 -0600

    Agregar la tabla con ids y logica incompleta js

--- p05/backend/script.js
+++ p05/backend/script.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+/* $(document).ready(() => {
     $('.card').on('click', () => {
         let id = $('.card').attr('id');
         console.log(id);
@@ -16,4 +16,32 @@ $(document).ready(() => {
             }
         });
     });
-});
+}); */
+
+function devolverCarta(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        //const je = '<img class="img-thumbnail img-fluid" src="./img/gato.png"></a>'
+        $.ajax({
+            url: "./server.php",
+            type: "GET",
+            data: { card: x },
+            success: function (response) {
+                console.log('Si entro');
+                resolve(response);
+            },
+            error: function () {
+                console.log('No funcionó');
+            }
+        });
+        
+      }, 2000);
+    });
+  }
+
+async function carta(id) {
+    document.getElementById(id).innerHTML = "Hiciste click";
+    console.log(id)
+    const a = await devolverCarta(id);
+    document.getElementById(id).innerHTML = '<img class="img-thumbnail img-fluid" src=./../img/'+a+'>'
+}
